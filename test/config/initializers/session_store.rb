0f7c70d87650b1e7644a5d40521df6c029e9404a
mike <michaxze@gmail.com>
2012-03-10 10:55:07 -0600

    change session store

--- test/config/initializers/session_store.rb
+++ test/config/initializers/session_store.rb
@@ -5,7 +5,7 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_rails_session',
+  :key         => '_bon_session',
   :secret      => 'd3e713f3693d02dfc627d694a7f974e36a03f331d64447bac90ff1d73755938667a99bfcef2037ecf8a40dfb3d6e4b2624d8aa2105466cbf1f7fb67a5889f1f8'
 }
 
